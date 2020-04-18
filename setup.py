06ccac3d15732e038aab3a4534bc0c8449731158
0-duke <0-duke@users.noreply.github.com>
2020-04-18 15:41:43 +0100

    updated repo url & contact email
    Updated the repo url with the original one and removed email contact address. Let's keep github as the only communication channel to keep visible track of requests.

--- setup.py
+++ setup.py
@@ -4,9 +4,8 @@ from setuptools import setup
 setup(name='wdpassport_utils',
       version='0.2',
       description='WD My Passport Drive Hardware Encryption Utility for Linux',
-      url='https://github.com/JoshData/wdpassport-utils',
+      url='https://github.com/0-duke/wdpassport-utils',
       author='0-duke, crypto-universe, JoshData',
-      author_email='jt@occams.info',
       license='GPLv2',
       install_requires=[
         'pyudev',
